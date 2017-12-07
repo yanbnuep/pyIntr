70136e6364a6db550b426d3f7df7d3a715686546
yanbnuep <yanbnuep@gmail.com>
2017-12-07 10:12:10 +0800

    news

--- news/models.py
+++ news/models.py
@@ -8,11 +8,11 @@ from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
 
 
-class Post(models.Model):
+class News(models.Model):
     title = models.CharField(max_length=50)
     content = RichTextField()
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.news_post
-admin.site.register(Post)
+        return self.News_text
+
