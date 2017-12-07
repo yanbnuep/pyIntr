50677b1e4f3fd63f6dee74977970f2f06114007e
yanbnuep <yanbnuep@gmail.com>
2017-12-07 11:08:32 +0800

    news

--- news/views.py
+++ news/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import News
+from django.template import loader
 
 def index(request):
-    return HttpResponse("hello,world")
+    newsList = News.objects.order_by('-created')[:5]
+    context = {
+        'newsList': newsList
+    }
+    return render(request,'news/index.html',context)
