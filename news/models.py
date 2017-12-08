7694c7d8997a06cff2d6c0108684d7a8ff94478e
yanbnuep <yanbnuep@gmail.com>
2017-12-09 06:49:59 +0800

    news

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from ckeditor_uploader.fields import  RichTextUploadingField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='static/media/coverImg')
+    cover = models.ImageField(upload_to='static/media/coverImg',blank=True, null=True)
     content = RichTextUploadingField(blank=True,null=True,verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
 
