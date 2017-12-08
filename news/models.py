4cb86ef4614b3a76ac446fde5aadbc75ab7c79a8
wuiancocos2d <wuiancocos2d@gmail.com>
2017-12-08 16:40:11 +0800

    news url

--- news/models.py
+++ news/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-from ckeditor_uploader.fields import  RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    content = RichTextUploadingField()
+    content = RichTextField()
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
