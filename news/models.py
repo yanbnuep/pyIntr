fc13e3063fac560667ef0a68229ae6100356a8ab
yanbnuep <yanbnuep@gmail.com>
2017-12-09 06:47:28 +0800

    news

--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ from ckeditor_uploader.fields import  RichTextUploadingField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
+    cover = models.ImageField(upload_to='static/media/coverImg')
     content = RichTextUploadingField(blank=True,null=True,verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
 
--- news/views.py
+++ news/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import News
-from django.template import loader
 
 def index(request):
     newsList = News.objects.order_by('-created')[:5]
