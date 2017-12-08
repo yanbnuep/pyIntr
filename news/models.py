4704364f5ae28fcfe603d54e08a68d71b738d8b5
wuiancocos2d <wuiancocos2d@gmail.com>
2017-12-08 16:46:53 +0800

    news url

--- news/models.py
+++ news/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from ckeditor_uploader.fieldsimport RichTextUploadingField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
