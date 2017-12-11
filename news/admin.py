0ead0517d3daff5eaccfbfa849de15ffc47f2ec8
yanbnuep <yanbnuep@gmail.com>
2017-12-11 17:12:23 +0800

    news

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import News
-from .models import Cover
 
-admin.site.register(News, Cover)
+admin.site.register(News)
--- news/models.py
+++ news/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-from django.contrib import admin
+
 # Create your models here.
 from ckeditor_uploader.fields import  RichTextUploadingField
 
-
-class Cover(admin.ModelAdmin):
-
-    def image_tag(self, obj):
-        return format_html('<img src="{}" />'.format(obj.image.url))
-
-    image_tag.short_description = 'Image'
-
-    list_display = ['image_tag',]
 
 class News(models.Model):
     title = models.CharField(max_length=50)
