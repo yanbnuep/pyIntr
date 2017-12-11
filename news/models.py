9b076167bfdfa2b6734eba4d04b8b588f4acf1e6
yanbnuep <yanbnuep@gmail.com>
2017-12-11 14:13:34 +0800

    news

--- news/models.py
+++ news/models.py
@@ -3,16 +3,12 @@ from django.db import models
 # Create your models here.
 from ckeditor_uploader.fields import  RichTextUploadingField
 
-class coverImage(models.Model):
-    cover = models.ImageField(upload_to='static/media/coverImg', blank=True, null=True)
-    def image_thumb(self):
-        return '<img src="/media/coverImg/%s" width="100" height="100" />' % (self.coverImg)
-        image_thumb.allow_tags = True
-
 class News(models.Model):
     title = models.CharField(max_length=50)
-    coverUpload = (('coverImage','image_thumb'))
-    thumbnail = ('image_thumb',)
+    cover = models.ImageField(upload_to='static/media/coverImg', blank=True, null=True)
+    def image_thumb(self):
+        return '<img src="/media/coverImg/%s" width="100" height="100" />' % (self.cover)
+        image_thumb.allow_tags = True
     content = RichTextUploadingField(blank=True,null=True,verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
 
