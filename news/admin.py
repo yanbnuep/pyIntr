740f468586cf3c7a6b1594f4b5b4a472dfdc3ca8
yanbnuep <yanbnuep@gmail.com>
2017-12-11 17:10:03 +0800

    news

--- news/admin.py
+++ news/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import News
-fields = ['image_tag']
-readonly_fields = ['image_tag']
+from .models import Cover
 
-admin.site.register(News)
+admin.site.register(News, Cover)
