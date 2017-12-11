8f0d17d2b58834e2c46e0885906a91cae7c52da0
yanbnuep <yanbnuep@gmail.com>
2017-12-11 17:55:24 +0800

    news

--- news/models.py
+++ news/models.py
@@ -1,18 +1,13 @@
 from django.db import models
-from django.utils.html import format_html
-from django.utils.safestring import mark_safe
+
 
 # Create your models here.
 from ckeditor_uploader.fields import  RichTextUploadingField
-
+from django_resized import ResizedImageField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='coverImg/', blank=True)
-
-    def image_tag(self):
-        return format_html("<br>{}</br>" , mark_safe("hello admin"))
-    image_tag.short_description = 'Text'
+    cover = models.ResizedImageField(upload_to='coverImg/', size=[500, 300])
     content = RichTextUploadingField(blank=True, null=True, verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
 
