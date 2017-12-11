bf7774acb51fd356474e81cc730c46d6ead2f899
yanbnuep <yanbnuep@gmail.com>
2017-12-11 17:25:45 +0800

    news

--- news/models.py
+++ news/models.py
@@ -11,7 +11,7 @@ class News(models.Model):
     cover = models.ImageField(upload_to='coverImg/', blank=True)
 
     def image_tag(self):
-        return mark_safe('<br>this url is %s</br>' % self.cover.url)
+        return mark_safe('<br>this url is %s</br>' % self.cover)
     image_tag.short_description = 'Image'
     content = RichTextUploadingField(blank=True, null=True, verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
