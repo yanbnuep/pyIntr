678c34e38e1bff2e1270c56f04d41de2649025cc
yanbnuep <yanbnuep@gmail.com>
2017-12-11 17:56:22 +0800

    news

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ from django_resized import ResizedImageField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    cover = models.ResizedImageField(upload_to='coverImg/', size=[500, 300])
+    cover = ResizedImageField(upload_to='coverImg/', size=[500, 300])
     content = RichTextUploadingField(blank=True, null=True, verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
 
