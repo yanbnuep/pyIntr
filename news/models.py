50e10dd11af03a1a1b987cf79fd55eede7362316
yanbnuep <yanbnuep@gmail.com>
2017-12-11 16:47:52 +0800

    news

--- news/models.py
+++ news/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
+from django.utils.safestring import mark_safe
 # Create your models here.
 from ckeditor_uploader.fields import  RichTextUploadingField
-from django.utils.safestring import mark_safe
+
+
 class News(models.Model):
     title = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='coverImg', blank=True, null=True)
-    def image_thumbnail(self):
-        return mark_safe('<br>show preview</br>')
-    image_thumbnail.short_description = 'Cover'
-    content = RichTextUploadingField(blank=True,null=True,verbose_name="context")
+    cover = models.ImageField(upload_to='/coverImg')
+
+    def image_tag(self):
+        return mark_safe('<img src="/directory/%s" width="150" height="150" />' % self.cover)
+    image_tag.short_description = 'Image'
+    content = RichTextUploadingField(blank=True, null=True, verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
