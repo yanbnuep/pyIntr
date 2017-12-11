e7077e301426c288dada88dbee08e59f4038f097
yanbnuep <yanbnuep@gmail.com>
2017-12-11 16:55:08 +0800

    news

--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ from ckeditor_uploader.fields import  RichTextUploadingField
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='coverImg/')
+    cover = models.ImageField(upload_to='coverImg/', blank=True)
 
     def image_tag(self):
         return mark_safe('<br>%s</br>' % self.cover)
