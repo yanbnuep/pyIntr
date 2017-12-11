1b1c16b7f4cf4f795c108818662f2e0333053ee8
yanbnuep <yanbnuep@gmail.com>
2017-12-11 16:19:30 +0800

    news

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ class News(models.Model):
     title = models.CharField(max_length=50)
     cover = models.ImageField(upload_to='coverImg', blank=True, null=True)
     def image_thumbnail(self):
-        return mark_safe('<br>show preview</br><img src="/media/%s" width="150" height="150" />' % (self.cover))
+        return mark_safe('<br>show preview</br>')
     image_thumbnail.short_description = 'Cover'
     content = RichTextUploadingField(blank=True,null=True,verbose_name="context")
     created = models.DateTimeField(auto_now_add=True)
